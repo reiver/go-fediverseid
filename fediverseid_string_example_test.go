886be4e780f40389cae2e84d19df673de9e81ee7
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-01-16 06:12:42 -0800

    fediverseid.FediverseID.String()

---
+++ fediverseid_string_example_test.go
@@ -0,0 +1,22 @@
+package fediverseid_test
+
+import (
+	"fmt"
+
+	"github.com/reiver/go-fediverseid"
+)
+
+func ExampleFediverseID_String() {
+
+	const name string = "joeblow"
+	const host string = "host.example"
+
+	fid := fediverseid.CreateFediverseID(name, host)
+
+	var fediverseID string = fid.String()
+
+	fmt.Printf("fediverse-id: %s", fediverseID)
+
+	// Output:
+	// fediverse-id: @joeblow@host.example
+}
